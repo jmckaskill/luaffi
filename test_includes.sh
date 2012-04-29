24046e9a57966017c0a6829a68ca6e175859b6b4
James R. McKaskill <james@foobar.co.nz>
2012-04-29 19:16:33 -0400

    remove inline functions in test_includes.sh

--- test_includes.sh
+++ test_includes.sh
@@ -4,6 +4,14 @@ mkdir -p test_includes
 for f in /usr/include/*.h
 do
   echo "${f}";
-  cc -E -c "${f}" | sed -e 's/^#.*//g' > "test_includes/`basename $f`"
+  (cc -E -c "${f}" || continue) | lua -e '
+    local str = io.read("*a")
+    -- remove preprocessor commands eg line directives
+    str = str:gsub("#[^\n]*", "")
+    -- remove inline function definitions and declarations
+    str = str:gsub("static [^;(]* __attribute__%b()", "static ")
+    str = str:gsub("static [^;(]*%b()%s*%b{}", "")
+    str = str:gsub("static [^;(]*%b()%s*;", "")
+    io.write(str)' > "test_includes/`basename $f`"
   lua -e 'ffi = require("ffi"); ffi.cdef(io.read("*a"))' < "test_includes/`basename $f`"
 done
