jmckaskill/luaffi

commit 0b0f2eed4597f0b09950e97b8540dff1cd3ba115

Support 8-, 16-, and 32-bit numeric types when parsing static const values
    static const int A = 1234;
    static const unsigned int B = 0x80000000;
    static const int8_t C = -1;
    static const uint8_t D = 0xff;
    static const int16_t E = -1;
    static const uint16_t F = 0xffff;
    static const int32_t G = -1;
    static const uint32_t H = 0xffffffff;

parser.c
@@ -2074,12 +2074,15 @@ static int parse_root(lua_State* L, struct parser* P)
             parse_typedef(L, P);
 
         } else if (IS_LITERAL(tok, "static")) {
+            struct ctype at;
+
             int64_t val;
             require_token(L, P, &tok);
             if (!IS_CONST(tok)) {
                 luaL_error(L, "expected 'static const int' on line %d", P->line);
             }
-            check_token(L, P, TOK_TOKEN, "int", "expected 'static const int' on line %d", P->line);
+
+            parse_type(L, P, &at);
 
             require_token(L, P, &tok);
             if (tok.type != TOK_TOKEN) {
@@ -2094,7 +2097,21 @@ static int parse_root(lua_State* L, struct parser* P)
 
             push_upval(L, &constants_key);
             lua_pushlstring(L, tok.str, tok.size);
-            lua_pushnumber(L, (int) val);
+
+            switch (at.type) {
+                case INT8_TYPE:
+                case INT16_TYPE:
+                case INT32_TYPE:
+                    if (at.is_unsigned)
+                        lua_pushnumber(L, (unsigned int) val);
+                    else
+                        lua_pushnumber(L, (int) val);
+                    break;
+
+                default:
+                    luaL_error(L, "expected a valid 8-, 16-, or 32-bit signed or unsigned integer type after 'static const' on line %d", P->line);
+            }
+
             lua_rawset(L, -3);
             lua_pop(L, 1); /*constants*/
 

commit ecf78484fb24b88b9f5123bd5d937c8c560cdf6a

Fix a stack overflow bug introduced in 0b0f2eed4597f0b09950e97b8540dff1cd3ba115

parser.c
@@ -2113,7 +2113,7 @@ static int parse_root(lua_State* L, struct parser* P)
             }
 
             lua_rawset(L, -3);
-            lua_pop(L, 1); /*constants*/
+            lua_pop(L, 2); /*constants and type*/
 
         } else {
             /* type declaration, type definition, or function declaration */